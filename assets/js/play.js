44014b265d58033fc0f567f8e29de1e631e435d0
amarocolas <mario.gesteira@spilgames.com>
2015-09-24 23:54:10 +0200

    teste com touch e empuxo na agua

--- assets/js/play.js
+++ assets/js/play.js
@@ -1,45 +1,61 @@
 var playState = {
 
 	create: function() {
-		sprite = game.add.sprite(32, 200, 'phaser');
-	    sprite.name = 'phaser-dude';
 
-	    game.physics.enable(sprite, Phaser.Physics.ARCADE);
-	    
+		this.forcas = {
+			forcaPraBaixo: 10,
+			empuxoDaAgua: .07
+		};
+
+		this.initialPosition = {
+			x:32,
+			y:200
+		};
+
+		this.player = game.add.sprite(this.initialPosition.x, this.initialPosition.y, 'phaser');
+	    this.player.name = 'phaser-dude';
+
+	    this.intouchdown = false;
+
+	    game.physics.enable(this.player, Phaser.Physics.ARCADE);
+
 	    group = game.add.group();
 	    group.enableBody = true;
 	    group.physicsBodyType = Phaser.Physics.ARCADE;
 
-
 	    cursors = game.input.keyboard.createCursorKeys();
-		
+
+	    /*
+		* coloca o jogo pra escutar se há mouse down ou touch
+	    */
+		game.input.onDown.add(function(){
+			this.intouchdown = true;
+		}, this);
+		game.input.onUp.add(function(){
+			this.intouchdown = false;
+		}, this);
+
+		window.player = this.player;
+	},
+
+	nadaPraBaixo: function() {
+
 	},
 
 	update: function() {
 		// game.physics.arcade.collide(sprite, group, this.collisionHandler, null, this);
-	    game.physics.arcade.overlap(sprite, group, this.collisionHandler, null, this);
+	    //game.physics.arcade.overlap(sprite, group, this.collisionHandler, null, this);
 
-	    sprite.body.velocity.x = 0;
-	    sprite.body.velocity.y = 0;
-
-	    if (cursors.left.isDown)
-	    {
-	        sprite.body.velocity.x = -200;
-	    }
-	    else if (cursors.right.isDown)
-	    {
-	        sprite.body.velocity.x = 200;
+	    if(this.intouchdown){
+	    	this.player.body.velocity.y += this.forcas.forcaPraBaixo;
 	    }
 
-	    if (cursors.up.isDown)
-	    {
-	        sprite.body.velocity.y = -200;
+	    if(this.player.body.position.y > this.initialPosition.y){
+	    	this.player.body.velocity.y -= (this.player.body.position.y - this.initialPosition.y) * this.forcas.empuxoDaAgua;
+	    }else if(this.player.body.position.y < this.initialPosition.y){
+	    	this.player.body.velocity.y = 0;
+	    	this.player.body.position.y = this.initialPosition.y;
 	    }
-	    else if (cursors.down.isDown)
-	    {
-	        sprite.body.velocity.y = 200;
-	    }
-
 
 	    if (game.rnd.frac() < 0.1) {
 	        var c = group.create(800, game.rnd.integerInRange(0, 570), 'veggies');
