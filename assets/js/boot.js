225c2239ebd004ff083bc2747321fd4eb71e4c32
amarocolas <mario.gesteira@spilgames.com>
2015-10-22 23:56:30 +0200

    FIX scaleSize method

--- assets/js/boot.js
+++ assets/js/boot.js
@@ -9,20 +9,7 @@ var bootState = {
 		game.stage.backgroundColor = '#000';
 		game.physics.startSystem(Phaser.Physics.ARCADE);
 
-		//game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
-
-		if(!game.device.desktop) {
-
-			game.scale.minWidth = 280;
-			game.scale.minHeight = 360;
-			game.scale.maxWidth = 1800;
-			game.scale.maxHeight = 1400;
-
-			game.scale.pageAlignHorizontally = true;
-			game.scale.pageAlignVertically = true;
-        	game.scale.setScreenSize(true);
-		}
-
+		game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
 		game.state.start('load');
 	}
 };
