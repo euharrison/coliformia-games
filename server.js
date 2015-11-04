6fe241ce4972427ac54fcae95ed8bf6ce67e7ec5
Harrison Mendonça <h@harrison.com.br>
2015-11-04 20:07:30 -0200

    add expressjs as webserver

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World! ' + process.env.TXT);
-});
+app.use('/', express.static(__dirname));
 
-var server = app.listen(process.env.PORT || 8888, function () {
+var server = app.listen(process.env.PORT || 8000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
 });
-
-app.use('/static', express.static('public'));
