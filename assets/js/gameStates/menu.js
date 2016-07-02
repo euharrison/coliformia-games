9442b439012810a8143d72f78fb0a45ee465220c
Harrison Mendonça <harrison@grama.cc>
2016-07-02 16:42:12 -0300

    add start screen

--- assets/js/gameStates/menu.js
+++ assets/js/gameStates/menu.js
@@ -1,11 +1,7 @@
-//splash screen do jogo
-
 var menuState = {
 
 	create: function() {
-		// How to start the game
-		var startLabel = game.add.text(game.world.centerX, game.world.height-80, 'touch para começar / touch para afundar', { font: '25px Arial', fill: '#ffffff' });
-		startLabel.anchor.setTo(0.5, 0.5);
+    game.add.image(0, 0, 'start-screen');
 
 		game.input.onUp.add(this.start, this);
 	},
