3c817423b737520a9279e12c2152c2869345d6e0
Harrison Mendonça <harrison@grama.cc>
2016-07-25 01:21:30 -0300

    update start screen

--- assets/js/gameStates/menu.js
+++ assets/js/gameStates/menu.js
@@ -3,6 +3,14 @@ var menuState = {
   create: function() {
     game.add.image(0, 0, 'start-screen');
 
+    var text = game.add.text(100, game.height-70, 'Touch the shitty\nwater to begin'.toUpperCase(), {
+      font: 'Noyh',
+      fill: '#ffffff',
+      fontSize: 44
+    });
+    text.lineSpacing = -10;
+    text.anchor.setTo(0, 1);
+
     game.input.onTap.add(this.start, this);
   },
 
