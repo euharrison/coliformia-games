bd11828bb3def74787dd3caee2bd1e7e4e7c9de8
Harrison Mendonça <harrison@grama.cc>
2016-07-25 00:31:34 -0300

    enable dynamic open graph tags

--- server.js
+++ server.js
@@ -1,11 +1,41 @@
 var express = require('express');
+var mustacheExpress = require('mustache-express');
 var app = express();
 
+//register file extension mustache
+app.engine('html', mustacheExpress());
+
+//register file extension for partials
+app.set('view engine', 'html'); 
+app.set('views', __dirname);
+
 app.use('/', express.static(__dirname));
+
+app.get('/:id', function(req, res) {
+  var index = Number(req.params.id) - 1;
+  var tags = [
+    {
+      title: 'Coliformia Games 1',
+      description: 'Uma descrição supimpa 1',
+      image: 'assets/img/facebook/big/01.jpg',
+    },
+    {
+      title: 'Coliformia Games 2',
+      description: 'Uma descrição supimpa 2',
+      image: 'assets/img/facebook/big/02.jpg',
+    },
+    {
+      title: 'Coliformia Games 3',
+      description: 'Uma descrição supimpa 3',
+      image: 'assets/img/facebook/big/03.jpg',
+    }
+  ]
+  res.render('index', tags[index]);
+});
 
 var server = app.listen(process.env.PORT || 8000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('App listening at http://%s:%s', host, port);
 });
