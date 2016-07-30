dc47926a14a935d3e04ad4ffb086a31536019d94
Harrison Mendonça <harrison@grama.cc>
2016-07-30 20:56:12 -0300

    fix dudu

--- assets/js/playState/levelItem/dudu.js
+++ assets/js/playState/levelItem/dudu.js
@@ -1,9 +1,9 @@
 var Dudu = function (game, play, group) {
   this.play = play;
-  Phaser.Sprite.call(this, game, game.width * 0.95, play.initialPosition.y + 10, 'dudu');
+  Phaser.Sprite.call(this, game, game.width-65, play.initialPosition.y+10, 'dudu');
 
+  this.animations.add('duduEntra', [0, 0, 0, 1, 2, 2], 12);
   this.animations.add('dudu', [2, 1, 0], 12);
-  this.animations.add('duduEntra', [0, 1, 2], 12);
   group.add(this);
 
   this.anchor.setTo(0.5,0.5);
@@ -16,17 +16,20 @@ Dudu.prototype = new LevelItem();
 Dudu.prototype.constructor = Dudu;
 
 Dudu.prototype.entra = function(){
-  var tween = this.game.add.tween(this.scale).to({x:1 , y:1}, 1000, Phaser.Easing.Elastic.In);
-  tween.onComplete.add(this.ataca, this);
-  tween.start();
-
   this.animations.play('duduEntra');
+
+  var tween = this.game.add.tween(this.scale).to({ x:1 , y:1 }, 500, Phaser.Easing.Back.Out, true);
+
+  this.game.time.events.add(Phaser.Timer.SECOND*1.5, this.fechaOlhos, this);
 };
 
-Dudu.prototype.ataca = function(){
+Dudu.prototype.fechaOlhos = function() {
   this.animations.play('dudu');
+  this.game.time.events.add(Phaser.Timer.SECOND*4/12, this.ataca, this);
+};
 
-  var laser = new Phaser.Image(game, -392, -32, 'dudu-laser');
+Dudu.prototype.ataca = function() {
+  var laser = new Phaser.Image(game, -261, -21, 'dudu-laser');
   this.addChild(laser);
 
   this.addBody(game, this.play, this.play.enemiesCollisionGroup, 'dudu');
