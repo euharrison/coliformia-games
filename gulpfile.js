e8fc60a8fa55244f37fbfcbb9503a82b19d4887d
amarocolas <mario.gesteira@spilgames.com>
2016-07-31 22:11:42 +0200

    CHANGE usando javascript minificado pelo gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,10 @@
 'use strict';
- 
+
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
- 
+
 gulp.task('sass', function () {
   gulp.src('./assets/scss/**/*.scss')
     .pipe(sourcemaps.init())
@@ -15,13 +15,27 @@ gulp.task('sass', function () {
 
 gulp.task('scripts', function() {
   return gulp.src([
-      './node_modules/phaser/dist/phaser.js',
-      './assets/js/boot.js',
-      './assets/js/load.js',
-      './assets/js/menu.js',
-      './assets/js/play.js',
-      './assets/js/gameover.js',
-      './assets/js/game.js',
+      './node_modules/phaser/build/phaser.min.js',
+      './assets/js/playState/sequenciador/pool.js',
+      './assets/js/playState/sequenciador/sequenciador.js',
+      './assets/js/playState/bg/paralaxBg.js',
+      './assets/js/playState/bg/bgScroller.js',
+      './assets/js/playState/bg/playerRastro.js',
+      './assets/js/playState/levelItem/levelItem.js',
+      './assets/js/playState/levelItem/bonner.js',
+      './assets/js/playState/levelItem/enemy.js',
+      './assets/js/playState/levelItem/dudu.js',
+      './assets/js/playState/levelItem/sewer.js',
+      './assets/js/playState/levelItem/powerups.js',
+      './assets/js/playState/player.js',
+      './assets/js/gameStates/boot.js',
+      './assets/js/gameStates/load.js',
+      './assets/js/gameStates/menu.js',
+      './assets/js/gameStates/play.js',
+      './assets/js/gameStates/revive.js',
+      './assets/js/gameStates/gameover.js',
+      './assets/js/gameConfigurations.js',
+      './assets/js/game.js'
     ])
     .pipe(sourcemaps.init())
     .pipe(concat('script.js'))
@@ -30,7 +44,7 @@ gulp.task('scripts', function() {
 });
 
 gulp.task('default', ['sass', 'scripts']);
- 
+
 gulp.task('watch', ['default'], function () {
   gulp.watch('./assets/scss/**/*.scss', ['sass']);
   gulp.watch('./assets/js/**/*.js', ['scripts']);
