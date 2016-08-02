585c5c1c7c9893693334aae7399fdb215bc98e55
Mario Montenegro Gesteira <Mario.Gesteira@spilgames.com>
2016-08-02 23:10:47 +0200

    Revert "Sons"

--- assets/js/gameStates/menu.js
+++ assets/js/gameStates/menu.js
@@ -12,18 +12,6 @@ var menuState = {
     text.anchor.setTo(0, 1);
 
     game.input.onTap.add(this.start, this);
-
-    game.coliformiaSounds = {};
-
-    game.coliformiaSounds.bolha_ou_pulo1 = game.add.audio('bolha_ou_pulo1');
-    game.coliformiaSounds.bolha_ou_pulo2 = game.add.audio('bolha_ou_pulo2');
-    game.coliformiaSounds.cai_na_agua = game.add.audio('cai_na_agua');
-    game.coliformiaSounds.coco_canhao = game.add.audio('coco_canhao');
-    game.coliformiaSounds.hit = game.add.audio('hit');
-    game.coliformiaSounds.morte_boiando = game.add.audio('morte_boiando');
-    game.coliformiaSounds.musica = game.add.audio('musica');
-    game.coliformiaSounds.nada = game.add.audio('nada');
-    game.coliformiaSounds.power_up = game.add.audio('power_up');
   },
 
   start: function() {
