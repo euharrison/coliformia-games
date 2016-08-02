902e5bb85446225b7f60661d1f46db22c9200ab5
Harrison Mendonça <harrison@grama.cc>
2016-08-02 03:37:08 -0300

    add share metatags

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.set('views', __dirname);
 app.get('/', function(req, res) {
   res.render('index',{
     title: 'Coliformia Games',
-    description: 'Uma descrição supimpa',
+    description: 'Sejam bem-vindos a maior competição de sobrevivência a coliformes fecais do mundo! Coliformia Games - Rip 2016! #RiodeMerda',
     image: '01.jpg',
   });
 });
@@ -21,19 +21,34 @@ app.get('/:id', function(req, res) {
   var index = Number(req.params.id) - 1;
   var tags = [
     {
-      title: 'Coliformia Games 1',
-      description: 'Uma descrição supimpa 1',
+      title: 'Coliformia Games :: Sebacitóide Bolhosa',
+      description: 'Peguei Sebacitóide Bolhosa na Baía de Guanabara! #RiodeMerda',
       image: '01.jpg',
     },
     {
-      title: 'Coliformia Games 2',
-      description: 'Uma descrição supimpa 2',
+      title: 'Coliformia Games :: Eczecrose Amebática',
+      description: 'Peguei Eczecrose Amebática na Lagoa Rodrigo de Freitas nadando mais de 100m! #RiodeMerda',
       image: '02.jpg',
     },
     {
-      title: 'Coliformia Games 3',
-      description: 'Uma descrição supimpa 3',
+      title: 'Coliformia Games :: Lepstosciforme Cutínica',
+      description: 'Peguei Lepstosciforme Cutínica na Praia de Copacabana nadando mais de 200m! #RiodeMerda',
       image: '03.jpg',
+    },
+    {
+      title: 'Coliformia Games :: Tifoidema Urticariante',
+      description: 'Peguei Tifoidema Urticariante na Baía de Guanabara nadando mais de 500m! #RiodeMerda',
+      image: '04.jpg',
+    },
+    {
+      title: 'Coliformia Games :: Ceraglifia Imunoglobótica',
+      description: 'Peguei Ceraglifia Imunoglobótica na Lagoa Rodrigo de Freitas nadando mais de 1000m! #RiodeMerda',
+      image: '05.jpg',
+    },
+    {
+      title: 'Coliformia Games :: Anidrosema Sebacitante',
+      description: 'Peguei Anidrosema Sebacitante na Praia de Copacabana nadando mais de 1500m! #RiodeMerda',
+      image: '06.jpg',
     }
   ]
   res.render('index', tags[index]);
